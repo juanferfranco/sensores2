db1389c361addc1b6c707228b4d5758c4d22fbca
Juan Franco <juanfranco@pop-os.localdomain>
2020-02-07 07:11:33 -0500

    add Unity project

---
+++ projects/RFIDReader/Assets/Ardity/Scripts/Samples/SampleRFIDProtocol.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text;
+
+public class SampleRFIDProtocol : MonoBehaviour
+{
+    public SerialControllerRFIDProtocol serialController;
+
+    // Initialization
+    void Start()
+    {
+        serialController = GameObject.Find("SerialController").GetComponent<SerialControllerRFIDProtocol>();
+        Debug.Log("Q: 0x21, W: 0x24, E: 0x2F, R: 0x22, T: 0x28, Y: 0x25");
+    }
+
+    // Executed each frame
+    void Update()
+    {
+       //---------------------------------------------------------------------
+        // Send data
+        //---------------------------------------------------------------------
+        if (Input.GetKeyUp(KeyCode.Q))
+        {
+            Debug.Log("Command 04 FF 21 19 95 ");
+            serialController.SendSerialMessage(new byte[] { 0x04, 0xFF, 0x21, 0x19, 0x95 });
+        }
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            Debug.Log("Command 05 00 24 00 25 29");
+            serialController.SendSerialMessage(new byte[] { 0x05, 0x00, 0x24, 0x00, 0x25, 0x29 });
+        }
+        if (Input.GetKeyUp(KeyCode.E))
+        {
+            Debug.Log("Command 05 00 2F 1E 72 34 ");
+            serialController.SendSerialMessage(new byte[] { 0x05, 0x00, 0x2F, 0x1E, 0x72, 0x34 });
+        }
+        if (Input.GetKeyUp(KeyCode.R))
+        {
+            Debug.Log("Command 06 00 22 31 80 E1 96 ");
+            serialController.SendSerialMessage(new byte[] { 0x06, 0x00, 0x22, 0x31, 0x80, 0xE1, 0x96 });
+        }
+        if (Input.GetKeyUp(KeyCode.T))
+        {
+            Debug.Log("Command 05 00 28 05 28 D7 ");
+            serialController.SendSerialMessage(new byte[] { 0x05, 0x00, 0x28, 0x05, 0x28, 0xD7 });
+        }
+        if (Input.GetKeyUp(KeyCode.Y))
+        {
+            Debug.Log("Command 05 00 25 0A A7 9F ");
+            serialController.SendSerialMessage(new byte[] { 0x05, 0x00, 0x25, 0x00, 0xFD, 0x30 });
+        }
+
+        //---------------------------------------------------------------------
+        // Receive data
+        //---------------------------------------------------------------------
+
+        byte[] message = serialController.ReadSerialMessage();
+
+        if (message == null)
+            return;
+        StringBuilder sb = new StringBuilder();
+        sb.Append("Packet: ");
+        foreach (byte data in message)
+        {
+            sb.Append(data.ToString("X2") + " ");
+        }
+        Debug.Log(sb);
+    }
+}
---
+++ projects/RFIDReader/Assets/Ardity/Scripts/Threads/SerialThreadRFIDProtocol.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+
+using System.Text;
+
+public class SerialThreadRFIDProtocol : AbstractSerialThread
+{
+    // Buffer where a single message must fit
+    private byte[] buffer = new byte[1024];
+    private int bufferUsed = 0;
+    private const int PRESET_VALUE = 0x0000FFFF;
+    private const int POLYNOMIAL = 0x00008408;
+
+
+    public SerialThreadRFIDProtocol(string portName,
+                                       int baudRate,
+                                       int delayBeforeReconnecting,
+                                       int maxUnreadMessages)                                       
+        : base(portName, baudRate, delayBeforeReconnecting, maxUnreadMessages, false)
+    {
+
+    }
+
+    protected override void SendToWire(object message, SerialPort serialPort)
+    {
+        byte[] binaryMessage = (byte[])message;
+        serialPort.Write(binaryMessage, 0, binaryMessage.Length);
+    }
+
+    protected override object ReadFromWire(SerialPort serialPort)
+    {
+        if(serialPort.BytesToRead > 0)
+        {
+            serialPort.Read(buffer, 0, 1);
+            bufferUsed = 1;
+            // wait for the rest of data
+            while ( bufferUsed < (buffer[0] + 1) )
+            {
+                bufferUsed = bufferUsed + serialPort.Read(buffer, bufferUsed, buffer[0]);
+            }
+
+            // Verify Checksum and
+            if(verifyChecksum(buffer) == true)
+            {
+                // send the package to the application
+                byte[] returnBuffer = new byte[bufferUsed];
+                System.Array.Copy(buffer, returnBuffer, bufferUsed);
+                bufferUsed = 0;
+                return returnBuffer;
+            }
+            else
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.Append("Packet: ");
+                foreach (byte data in buffer)
+                {
+                    sb.Append(data.ToString("X2") + " ");
+                }
+                sb.Append("Checksum fails");
+                Debug.Log(sb);
+                return null;
+            }
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    private bool verifyChecksum(byte[] packet)
+    {
+        bool checksumOK = false;
+        byte ucI, ucJ;
+        int uiCrcValue = PRESET_VALUE;
+        int len = packet[0] + 1;
+
+        for (ucI = 0; ucI < (len - 2); ucI++)
+        {
+            uiCrcValue = uiCrcValue ^ packet[ucI];
+            for (ucJ = 0; ucJ < 8; ucJ++)
+            {
+                if ( (uiCrcValue & 0x00000001) == 0x00000001)
+                {
+                    uiCrcValue = (uiCrcValue >> 1) ^ POLYNOMIAL;
+                }
+                else
+                {
+                    uiCrcValue = (uiCrcValue >> 1);
+                }
+            }
+        }
+        int LSBCkecksum = uiCrcValue & 0x000000FF;
+        int MSBCkecksum = (uiCrcValue & 0x0000FF00) >> 8 ;
+
+        if ((packet[len - 2] == LSBCkecksum) && (packet[len - 1] == MSBCkecksum)) checksumOK = true;
+        return checksumOK;
+    }
+
+
+}
