98e9167a44a4aaa5e6d07f6f52f574702aadbd53
Juan Fernando Franco <juanfh@pop-os.localdomain>
2020-02-27 11:23:47 -0500

    add Unity project parcial1 mejora

---
+++ projects/sens2Par1Mejora/Assets/Scripts/App.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text;
+
+public class App : MonoBehaviour
+{
+    public Controller serialController;
+    private float timer = 0.0f;
+    private float waitTime = 0.005f;
+
+    private Transform objTransform;
+    private Vector3 scaleChange;
+
+
+    // Initialization
+    void Start()
+    {
+        serialController = GameObject.Find("SerialController").GetComponent<Controller>();
+        objTransform = GetComponent<Transform>();
+        scaleChange = new Vector3(0f, 0f, 0f);
+    }
+
+    // Executed each frame
+    void Update()
+    {
+
+      //---------------------------------------------------------------------
+        // Send data
+        //---------------------------------------------------------------------
+        if (Input.GetKeyUp(KeyCode.Q))
+        {
+            //Debug.Log("Get data 0x73 ");
+            serialController.SendSerialMessage(new byte[] { 0x73});
+        }
+
+        timer += Time.deltaTime;
+        if (timer > waitTime)
+        {
+            timer = timer - waitTime;
+
+            serialController.SendSerialMessage(new byte[] { 0x73});
+        }
+
+
+
+        //---------------------------------------------------------------------
+        // Receive data
+        //---------------------------------------------------------------------
+
+        byte[] message = serialController.ReadSerialMessage();
+
+        if (message == null)
+            return;
+
+        float x = ((float)System.BitConverter.ToUInt16(message, 0) ) / 500F;
+        float y = ((float)System.BitConverter.ToUInt16(message, 2) ) / 500F;
+        float z = ((float)System.BitConverter.ToUInt16(message, 4) ) / 500F;
+        scaleChange.Set(x,y,z);
+
+        objTransform.localScale =  scaleChange;
+
+/*         StringBuilder sb = new StringBuilder();
+        sb.Append("Packet: ");
+        foreach (byte data in message)
+        {
+            sb.Append(data.ToString("X2") + " ");
+        }
+        Debug.Log(sb); */
+    }
+}
---
+++ projects/sens2Par1Mejora/Assets/Scripts/Protocol.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+
+using System.Text;
+
+public class Protocol : AbstractSerialThread
+{
+    // Buffer where a single message must fit
+    private byte[] buffer = new byte[1024];
+    private int bufferUsed = 0;
+
+    public Protocol(string portName,
+                                      int baudRate,
+                                      int delayBeforeReconnecting,
+                                      int maxUnreadMessages)
+        : base(portName, baudRate, delayBeforeReconnecting, maxUnreadMessages, false)
+    {
+
+    }
+
+    protected override void SendToWire(object message, SerialPort serialPort)
+    {
+        byte[] binaryMessage = (byte[])message;
+        serialPort.Write(binaryMessage, 0, binaryMessage.Length);
+    }
+
+    protected override object ReadFromWire(SerialPort serialPort)
+    {
+        if(serialPort.BytesToRead >= 6)
+        {
+
+            bufferUsed = serialPort.Read(buffer, 0, 6);
+            byte[] returnBuffer = new byte[bufferUsed];
+            System.Array.Copy(buffer, returnBuffer, bufferUsed);
+/*
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Packet: ");
+            foreach (byte data in buffer)
+            {
+                sb.Append(data.ToString("X2") + " ");
+            }
+            sb.Append("Checksum fails");
+            Debug.Log(sb);
+*/
+
+            return returnBuffer;
+        }
+        else
+        {
+            return null;
+        }
+    }
+}
